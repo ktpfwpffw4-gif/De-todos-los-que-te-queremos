df82d63b3920bfe4e972344e06f6f3d9c7a09b43
ktpfwpffw4-gif <ktpfwpffw4@privaterelay.appleid.com>
2026-06-18 01:15:30 -0600

    Set firebaseDatabaseURL for Firebase Realtime Database
    Updated the firebaseDatabaseURL to point to the actual Realtime Database.

--- firebase-config.example.js
+++ firebase-config.example.js
@@ -1,6 +1,5 @@
 // Copia este archivo a `firebase-config.js` y añade la URL de tu
 // Realtime Database de Firebase. No subas `firebase-config.js` al repo.
 window.CARTAS_DB_CONFIG = {
-  // Ejemplo: https://mi-proyecto-default-rtdb.firebaseio.com
-  firebaseDatabaseURL: ""
+  firebaseDatabaseURL: "https://cartas-darlyn-default-rtdb.firebaseio.com"
 };
