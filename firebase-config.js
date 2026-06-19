d01addc78b3f2155464993c57dc0d2d61399bbb3
ktpfwpffw4-gif <ktpfwpffw4@privaterelay.appleid.com>
2026-06-18 22:33:11 -0600

    Agregar configuración de Firebase Storage

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,11 @@
 window.CARTAS_DB_CONFIG = {
-  firebaseDatabaseURL: "https://cartas-darlyn-default-rtdb.firebaseio.com"
+  firebaseDatabaseURL: "https://cartas-darlyn-default-rtdb.firebaseio.com",
+  firebaseConfig: {
+    apiKey: "AIzaSyBnKqY_TIvVvJ5vJ_KxY8Z4pK7mL8nN9oP",
+    authDomain: "cartas-darlyn.firebaseapp.com",
+    projectId: "cartas-darlyn",
+    storageBucket: "cartas-darlyn.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
+  }
 };
